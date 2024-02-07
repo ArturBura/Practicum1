36d708c700afab2e41cb3e9e869c9b797cc21a61
ArturBura <154540623+ArturBura@users.noreply.github.com>
2024-02-07 07:02:33 +0200

    Add files via upload

---
+++ Практикум 16/Практикум 16/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 1
+//for (int a = 1; a <= 10; a++)
+//{
+//    string b = "a";
+////    Console.Write($" {b.ToUpper()}");
+////}
+//// Задача 2
+////for (int b = 50, a = 40; b >= a; b--)
+////{
+////    Console.WriteLine(b);
+////}
+////Задача 3
+//for (int b = 6; b <= 36; b += 6)
+//    if (b != 18)
+//    {
+//        {
+//            Console.Write($" {b}");
+//        }
+//    }
+//Console.ReadKey();
+////Задача 4
+////int member = 0;
+////Console.Write($"Числа кратные 11:");
+////for (int i = 1; i <= 99; i += 1)
+////{
+////    if (i % 11 == 0)
+////    {
+////        Console.Write($"\t{i}");
+////        member += 1;
+////    }
+////}
+////Console.WriteLine($"\nКоличество:{member}");
